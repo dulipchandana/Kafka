43a43f2e247299c3bbb104dcec19435bbc8200f2
Dulip <dcdulip@gmail.com>
2022-05-23 23:52:06 +1000

    add kafka changes

---
+++ confluent-kafka/src/test/java/io/confluent/developer/confluentkafka/ConfluentKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.confluent.developer.confluentkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfluentKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
